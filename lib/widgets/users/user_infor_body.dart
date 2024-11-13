7d124a0d366268d2a57dd833139b0e056b41533a
Cao Ky Anh <caokyanh122@gmail.com>
2024-11-13 21:24:14 +0700

    70%-13,14/11

--- lib/widgets/users/user_infor_body.dart
+++ lib/widgets/users/user_infor_body.dart
@@ -1,7 +1,7 @@
 import 'package:final_project_rent_moto_fe/screens/auth/login/login_screen.dart';
 import 'package:final_project_rent_moto_fe/screens/dashboard.dart';
 import 'package:final_project_rent_moto_fe/widgets/auth/button_auth.dart';
-import 'package:final_project_rent_moto_fe/widgets/auth/users/user_infor_myaccount_form.dart';
+import 'package:final_project_rent_moto_fe/widgets/users/user_infor_myaccount_form.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
